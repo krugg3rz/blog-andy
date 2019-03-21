bb3b5c6fcddf0b2d2b641a69314ea099278cc930
Andy Ford <andy.ford@Nology.local>
2019-03-21 16:25:59 +0000

    Time output added but needs to be formatted

---
+++ main.ts
@@ -0,0 +1,37 @@
+class BlogPost {
+    title: string;
+    content: string;
+    private _date: Date;
+
+    public constructor(title: string, content: string) {
+        this.title = title;
+        this.content = content;
+        this._date = new Date();
+    }
+
+    get date(){
+        return this._date;
+    };
+}
+
+let blogTitles: BlogPost[] = [];
+
+const createBlogPost = () => {
+    let currentTime = new Date();
+    let form = document.getElementById("blog-form");
+    let title = form["blog-title"].value;
+    let content = form["blog-post"].value;
+    blogTitles.push(new BlogPost(title, content));
+    populateBlogPosts();
+}
+
+const printBlogPost = (blogPost: BlogPost) => {
+    document.getElementById("blog-output").innerHTML += "<h2>" + blogPost.title + "</h2>";
+    document.getElementById("blog-output").innerHTML += "<h4>" + blogPost.content + "</h4>";
+    document.getElementById("blog-output").innerHTML += "<h5>" + blogPost.date + "</h5>" + "-------";
+}
+
+const populateBlogPosts = () => {
+    document.getElementById("blog-output").innerHTML = ""
+    blogTitles.forEach(printBlogPost);
+}
