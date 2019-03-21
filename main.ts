95635b299c69c584031e79dcc977f7bd224a299d
Andy Ford <andy.ford@Nology.local>
2019-03-21 11:17:20 +0000

    Got data going into empty array

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
-const sayHello = () => {
-    alert('Hello!');
+class BlogPost {
+    title: string;
+    content: string;
+
+    public constructor(title: string, content: string) {
+        this.title = title;
+        this.content = content;
+    }
+
+    getBlog() {
+        return this.title + " " + this.content;
+    }
+
+    
+}
+
+let blogTitles : BlogPost[] = [];
+
+const createBlogPost = () => {
+    let form = document.getElementById("blog-form");
+    let title = form["blog-title"].value;
+    let content = form["blog-post"].value;
+    blogTitles.push(new BlogPost(title, content));
+    console.log(blogTitles);
 } 
+
